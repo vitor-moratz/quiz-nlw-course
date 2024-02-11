37c6d7d732ee467ae310114e8b65269f1a76d829
vitor-moratz <vitor.morato80@gmail.com>
2024-02-10 21:00:13 -0300

    Tratando estilizacao com CSS e finalizando o projeto

--- src/js/script.js
+++ src/js/script.js
@@ -94,6 +94,11 @@ const perguntas = [
 const quiz = document.querySelector('#quiz')
 const template = document.querySelector('template')
 
+const corretas = new Set()
+const totalDePerguntas = perguntas.length
+const mostrarTotal = document.querySelector('#acertos span')
+mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
+
 for(const item of perguntas) {
     const quizItem = template.content.cloneNode(true)
     quizItem.querySelector('h3').textContent = item.pergunta
@@ -101,7 +106,20 @@ for(const item of perguntas) {
     for(let resposta of item.respostas) {
         const dt = quizItem.querySelector('dl dt').cloneNode(true)
         dt.querySelector('span').textContent = resposta
+        dt.querySelector('input').setAttribute('name', 'pergunta-' + perguntas.indexOf(item))
+        dt.querySelector('input').value = item.respostas.indexOf(resposta)
+        dt.querySelector('input').onchange = (event) => {
+            const estaCorreta = event.target.value == item.correta
+            corretas.delete(item)
+            if(estaCorreta) {
+                corretas.add(item)
+            }
 
+            mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
+        }
+        
+        
+        
         quizItem.querySelector('dl').appendChild(dt)
     }
 
